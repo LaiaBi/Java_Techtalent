4ea9a333c86fed0d93fe04d6907df74916ed3bf4
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-05 10:56:22 +0100

    Suma prueba1

--- Prueba1/src/Operador.java
+++ Prueba1/src/Operador.java
@@ -1,8 +1,11 @@
 public class Operador {
 			public static void main(String[] args) {	
-				int operador=10;
-				System.out.println(operador);
-				int operador++;
-			
+				int x=0;
+				Operador++; //Operador=11
+				Operador++; //Operador=12
+				Operador--; //Operador=11
+				
+				x=Operador++; //x=11, Operador=12
+				x=++Operador; //x=13, Operador=13
 			}
 }
---
+++ Prueba1/src/suma.java
@@ -0,0 +1,10 @@
+public class suma {
+	public static void main(String[] args) {
+        // TODO Auto-generated method stub
+		int a=30;
+		double b=30.5;
+		double suma=a+b; // Si fuera un int daria error
+		System.out.println(suma);
+	}
+
+}
