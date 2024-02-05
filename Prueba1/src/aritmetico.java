290fa701fee5ae0e9b01d625511f943505235b2b
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-05 11:14:35 +0100

    Create aritmetico.java
    La diferencia de el símbolo

---
+++ Prueba1/src/aritmetico.java
@@ -0,0 +1,17 @@
+
+public class aritmetico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int operador1=20;
+		int operador2=15;
+		int resultado=0;
+		resultado= operador1+operador2; //resultado=35
+		resultado= operador1-operador2; //resultado=5
+		resultado= operador2-operador1; //resultado=-5
+		resultado= operador1*operador2; //resultado=300
+		resultado= operador1/operador2; //resultado=1 (como es int no incluye decimales)
+		resultado= operador1%operador2; //resultado=5 (el resto de la division)
+	}
+
+}
