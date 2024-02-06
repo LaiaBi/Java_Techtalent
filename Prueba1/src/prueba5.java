eda5499f6470176ee0ff8f745ef4685f190205cb
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-06 09:16:39 +0100

    Update prueba5.java

--- Prueba1/src/prueba5.java
+++ Prueba1/src/prueba5.java
@@ -1,4 +1,28 @@
-
 public class prueba5 {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declarar variable
+		int A;
+		int B;
+		int C;
+		int D;
+		//valores
+		A=5;
+		System.out.println(A);
+		B=7;
+		System.out.println(B);
+		C=3;
+		System.out.println(C);
+		D=1;
+		System.out.println(D);
+		//tomar el valor
+		B=C;
+		System.out.println(B);
+		C=A;
+		System.out.println(C);
+		A=D;
+		System.out.println(A);
+		D=B;
+		System.out.println(D);
+	}
 }
