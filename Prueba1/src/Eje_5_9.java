99f4c9f6d80670974a0fe0296310f05056ce8b93
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-08 10:24:21 +0100

    Create Eje_5_9.java

---
+++ Prueba1/src/Eje_5_9.java
@@ -0,0 +1,13 @@
+public class Eje_5_9 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3.
+		
+		for (int num = 1; num <=100; num++) {
+			if (num % 2 == 0 && num % 3 == 0) {		
+			System.out.println(num);	
+	     }
+	  }
+   }	
+}
