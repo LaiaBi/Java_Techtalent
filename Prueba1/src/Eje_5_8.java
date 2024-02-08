d3f58afdf6cdeb0494079b7dcc79ef7da811e798
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-08 10:21:30 +0100

    Create Eje_5_8.java

---
+++ Prueba1/src/Eje_5_8.java
@@ -0,0 +1,13 @@
+public class Eje_5_8 {
+	public static void main(String[] args) {
+        // TODO Auto-generated method stub
+		
+		//Muestra los números del 1 al 100 (ambos incluidos).
+	
+		int num = 1;
+		
+			for (int num1= 1; num1 <= 100; num1++) {
+			System.out.println(num1);
+			}
+		}
+}
