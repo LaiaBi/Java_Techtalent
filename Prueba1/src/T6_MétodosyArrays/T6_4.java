34a8301592870b68f8c953de4ea0bd96b6ca49b2
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-21 11:42:31 +0100

    .

--- Prueba1/src/T6_MétodosyArrays/T6_4.java
+++ Prueba1/src/T6_MétodosyArrays/T6_4.java
@@ -1,10 +1,16 @@
 package T6_MétodosyArrays;
-
+import java.util.Scanner;
 public class T6_4 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		public static void main(String[] args) {
+	        // Leer la entrada del teclado
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        // Solicitar un número por teclado
+	        System.out.print("Porfavor,Introduzca un número para calcular: ");
+	    
+	        // Leer el número ingresado
+	        int numero = scanner.nextInt();
 	}
 
 }
---
+++ Prueba1/src/T6_MétodosyArrays/T6_8.java
@@ -0,0 +1,19 @@
+package T6_MétodosyArrays;
+import java.util.Scanner;
+public class T6_8 {
+
+	public static void main(String[] args) {
+		
+		//seleccionar los valores
+		int matriz[]=new int [10];
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Porfavor,da un valor: ");
+		
+		//asignacion de posiciones
+		for (int i=0; i<10; i++){
+			int num = scanner.nextInt();
+			matriz[i]=num;
+		}
+		
+	}
+}
