e39095b5a1a574217de4129b04deb752e6a9cf88
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-22 12:55:09 +0100

    .

--- Prueba1/src/T6_MétodosyArrays/T6_7.java
+++ Prueba1/src/T6_MétodosyArrays/T6_7.java
@@ -1,43 +1,43 @@
 package T6_MétodosyArrays;
 import java.util.Scanner;
-public class T6_7 {
-	public static void main(String[] args) {
-		
-		Scanner sc= new Scanner(System.in); 
-		System.out.println("Introduce la cantidad de €"); 
-		double euro = sc.nextDouble(); 
-		
-		Scanner sc1= new Scanner(System.in); 
-		System.out.println("Indica la moneda a la que deseas hacer el cambio: (Dolar, Yen o Libra)"); 
-		String moneda= sc.nextLine(); 
-		
-		double qConvertida =conversor(euro,moneda); 
-		
-		System.out.println("La cantidad " +euro +" convertida a "+moneda + " es: "+qConvertida ); 
-		
-		sc.close();
-	}
 
-	public static double conversor (double q,String m) {
-		double monedas= 0; 
-		
-		switch (m.toLowerCase()) {
-		
-		case "dolar": 
-			monedas = q*1.28611;
-			break;
-			
-		case "yen": 
-			monedas= q*129.852; 
-			break;
-			
-		case "libra": 
-			monedas =q*0.86; 
-			break; 
-		default: 
-			System.out.println("Moneda introducida no válida");
-		}
-		return monedas; 
-			
-	}
+public class T6_7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce la cantidad de €");
+        double euro = sc.nextDouble();
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Indica la moneda a la que deseas hacer el cambio: (Dolar, Yen o Libra)");
+        String moneda = sc1.next(); // Cambiado de sc.nextLine() a sc.next()
+
+        double qConvertida = conversor(euro, moneda);
+
+        System.out.println("La cantidad " + euro + " convertida a " + moneda + " es: " + qConvertida);
+
+        sc.close();
+    }
+    public static double conversor(double q, String m) {
+        double monedas = 0;
+
+        switch (m.toLowerCase()) {
+
+            case "dolar":
+                monedas = q * 1.28611;
+                break;
+
+            case "yen":
+                monedas = q * 129.852;
+                break;
+
+            case "libra":
+                monedas = q * 0.86;
+                break;
+                
+            default:
+                System.out.println("Moneda introducida no válida");
+        }
+        return monedas;
+    }
 }
