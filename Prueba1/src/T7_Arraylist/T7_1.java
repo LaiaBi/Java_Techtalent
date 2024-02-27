f0bfd19cb3f9e1d6223845cf48dc9a6631a6c87c
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-27 12:41:06 +0100

    Update T7_1.java

--- Prueba1/src/T7_Arraylist/T7_1.java
+++ Prueba1/src/T7_Arraylist/T7_1.java
@@ -1,24 +1,38 @@
 package T7_Arraylist;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 public class T7_1 {
 	public static void main(String[] args) {
-	// Crear un HashMap con clave de tipo String y valor de tipo Integer
-        HashMap<String, Integer> notasAlumnos = new HashMap<>();
-   
-    // Agregar elementos al HashMap
-        notasAlumnos.put("Lupita", 3,4,5,8);
-        notasAlumnos.put("Eduardo", 6,7,8,9);
-        notasAlumnos.put("Juan", 10,8,6,9);
-        notasAlumnos.put("Victoria", 5,7,8,6);
-	}
-	// Iterar sobre las claves del HashMap
-    System.out.println("Claves en el notasAlumnos:");
-    for (String nombre : notasAlumnos.keySet()) {
-        System.out.println(nombre);
-    }
-    // Iterar sobre los valores del HashMap
-    System.out.println("Valores en el notasAlumnos:");
-    for (int notas : notasAlumnos.values()) {
-        System.out.println(notas);
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la cantidad de alumnos: ");
+        int cantidadAlumnos = scanner.nextInt();
+
+        Map<String, Double> notasAlumnos = new HashMap<>();
+        //Indicar las Key(alumnos)
+        for (int i = 1; i <= cantidadAlumnos; i++) {
+            System.out.print("Ingrese el nombre del alumno " + i + ": ");
+            String nombreAlumno = scanner.next();
+        //Indicar las Value(nota)
+            System.out.print("Ingrese la nota " + i + ": ");
+            double nota = scanner.nextDouble();
+
+        // Almacenar la nota en el HashMap
+            notasAlumnos.put(nombreAlumno, nota);
+        }
+
+        double sumaNotas = 0;
+
+        // Calcular la suma de las notas
+        for (double nota : notasAlumnos.values()) {
+            sumaNotas += nota;
+        }
+
+        double notaMedia = sumaNotas / cantidadAlumnos;
+
+        System.out.println("La nota media de los " + cantidadAlumnos + " alumnos es: " + notaMedia);
+
+        scanner.close();
     }
 }
