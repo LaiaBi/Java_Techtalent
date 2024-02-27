9d41f1b5d011fc1b07990ce6e64bb29729ffbd62
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-27 09:04:53 +0100

    .

--- Prueba1/src/T6_MétodosyArrays/T6_10.java
+++ Prueba1/src/T6_MétodosyArrays/T6_10.java
@@ -8,32 +8,24 @@ public class T6_10 {
 		def es_primo(numero):
 		    if numero < 2:
 		        return False
-		    for i in range(2, int(numero**0.5) + 1);
-		        if numero % i == 0;
+		    for i in range(2, int(numero**0.5) + 1):
+		        if numero % i == 0:
 		            return False
 		    return True
-		    		def generar_primos(tamano):
-		    		    primos = []
-		    		    while len(primos) < tamano:
-		    		        numero = random.randint(1, 100)
-		    		        if es_primo(numero) and numero not in primos:
-		    		            primos.append(numero)
+
+		def generar_primos(tamano):
+		    primos = [random.choice([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]) for _ in range(tamano)]
 		    return primos
 
-		    		def encontrar_mayor(array):
-		    		    return max(array)
+		if __name__ == "__main__":
+		    try:
+		        tamano_array = int(input("Ingrese el tamaño del array de números primos: "))
+		        numeros_primos = generar_primos(tamano_array)
 
-		    		if __name__ == "__main__":
-		    		    try:
-		    		        tamano_array = int(input("Ingrese el tamaño del array de números primos: "))
-		    		        numeros_primos = generar_primos(tamano_array)
+		        print(f"\nNúmeros primos generados: {numeros_primos}")
 
-		    		        print(f"\nNúmeros primos generados: {numeros_primos}")
+		        mayor_primo = max(numeros_primos)
+		        print(f"\nEl número primo más grande es: {mayor_primo}")
 
-		    		        mayor_primo = encontrar_mayor(numeros_primos)
-		    		        print(f"\nEl número primo más grande es: {mayor_primo}")
-
-		    		    except ValueError:
-		    		        print("Por favor, ingrese un valor válido para el tamaño del array.")
-	}
-}
+		    except ValueError:
+		        print("Por favor, ingrese un valor válido para el tamaño del array.")
--- Prueba1/src/T6_MétodosyArrays/T6_12.java
+++ Prueba1/src/T6_MétodosyArrays/T6_12.java
@@ -1,5 +1,17 @@
 package T6_MétodosyArrays;
 
 public class T6_12 {
+	function filtrarDigitos(arr, numero) {
+	    let nuevoArray = arr.filter(x => x.toString().endsWith(numero.toString()));
+	    return nuevoArray;
+	}
+
+	// Ejemplo de uso
+	let tamanio = 10;
+	let miArray = [123, 456, 789, 321, 654, 987, 111, 222, 333, 444];
+	let numeroIndicado = 4;
+
+	let resultado = filtrarDigitos(miArray, numeroIndicado);
+	console.log(resultado);
 
 }
