c42b127a54a3fcbd530f7f17a02aeb57f115c82f
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-29 13:34:57 +0100

    .

--- Prueba1/src/T7_Arraylist/T7_2.java
+++ Prueba1/src/T7_Arraylist/T7_2.java
@@ -1,5 +1,17 @@
 package T7_Arraylist;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 public class T7_2 {
-
+	public static void main(String[] args) {
+		//Ingresar cantidad que hay en el objeto inicial
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de productos de el carro: ");
+        int cantidadProductos = scanner.nextInt();
+        //calcular IVA 
+        int precioProducto=20;
+        System.out.println("El precio en bruto,  es "+(precioProducto));
+		final double IVA= 0.21;System.out.println("El precio del producto, incluyendo el IVA, es "+(precioProducto+(precioProducto*IVA)));
+		//
+	}
 }
