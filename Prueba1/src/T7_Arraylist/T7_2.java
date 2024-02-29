b4249606298ff1418a7630bd0f289bb654eb7097
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-02-29 13:56:04 +0100

    Update T7_2.java

--- Prueba1/src/T7_Arraylist/T7_2.java
+++ Prueba1/src/T7_Arraylist/T7_2.java
@@ -4,14 +4,16 @@ import java.util.Map;
 import java.util.Scanner;
 public class T7_2 {
 	public static void main(String[] args) {
-		//Ingresar cantidad que hay en el objeto inicial
+	//Ingresar cantidad que hay en el objeto inicial
 		Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese la cantidad de productos de el carro: ");
         int cantidadProductos = scanner.nextInt();
-        //calcular IVA 
+    //calcular IVA 
         int precioProducto=20;
         System.out.println("El precio en bruto,  es "+(precioProducto));
-		final double IVA= 0.21;System.out.println("El precio del producto, incluyendo el IVA, es "+(precioProducto+(precioProducto*IVA)));
-		//
+		final double IVA= 0.21;
+		System.out.println("El precio del producto, incluyendo el IVA, es "+(precioProducto+(precioProducto*IVA)));
+	//Pagar y cobrar
+		
 	}
 }
