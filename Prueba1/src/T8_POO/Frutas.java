3b320f3b2a1997a14b5cc145ab07ceddb3ad703b
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-03-07 10:26:18 +0100

    frutas/objeto

---
+++ Prueba1/src/T8_POO/Frutas.java
@@ -0,0 +1,20 @@
+package T8_POO;
+
+import javax.swing.Spring;
+
+public class Frutas {
+	// Atributos
+	Spring nombre;
+	Spring color;
+	double precio;
+
+	// Constructores
+
+	// Métodos
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
