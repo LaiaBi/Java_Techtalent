bf1901d63b2226d8670895d165dd798d141e1588
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-03-07 12:03:17 +0100

    Update Frutas.java

--- Prueba1/src/T8_POO/Frutas.java
+++ Prueba1/src/T8_POO/Frutas.java
@@ -9,7 +9,29 @@ public class Frutas {
 	double precio;
 
 	// Constructores
+public Frutas () {
+	this.nombre="";
+	this.color="";
+	this.precio=0,0
+	}
 
+public Frutas (String param1) {
+	this.nombre=param1;
+	this.color="";
+	this.precio=0,0
+	}
+
+public Frutas (String param1, double param2) {
+	this.nombre=param1;
+	this.color=param2;
+	this.precio=0,0
+	}
+
+public Frutas (String param1, sting param2, double param3) {
+	this.nombre=param1;
+	this.color=param2;
+	this.precio=param3
+	}
 	// Métodos
 
 	public static void main(String[] args) {
