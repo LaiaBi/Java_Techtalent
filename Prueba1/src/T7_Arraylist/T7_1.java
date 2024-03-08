70ffd1381a015548cd6afdc6dbfdd7c2210d64cc
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-03-08 10:41:28 +0100

    Update T7_1.java

--- Prueba1/src/T7_Arraylist/T7_1.java
+++ Prueba1/src/T7_Arraylist/T7_1.java
@@ -1,40 +1,46 @@
-package T7_Arraylist;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
+
 public class T7_1 {
+
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("Ingrese la cantidad de alumnos: ");
-		int cantidadAlumnos = scanner.nextInt();
+		
+			Scanner scanner = new Scanner(System.in);
 
-		Map<String, Double> notasAlumnos = new HashMap<>();
-		// Indicar las Key(alumnos)
-		for (int i = 1; i <= cantidadAlumnos; i++) {
-			System.out.print("Ingrese el nombre del alumno " + i + ": ");
-			String nombreAlumno = scanner.next();
-			// Indicar las Value(nota)
-			System.out.print("Ingrese la nota " + i + ": ");
-			double nota = scanner.nextDouble();
+			System.out.print("Ingrese la cantidad de alumnos: ");
+			int totalNotas = scanner.nextInt();
 
-			// Almacenar la nota en el HashMap
-			notasAlumnos.put(nombreAlumno, nota);
+			Map<String, Double> notasAlumnos = new HashMap<>();
+			// Indicar las Key(alumnos)
+			for (int i = 1; i <= totalNotas; i++) {
+				System.out.print("Ingrese el nombre del alumno " + i + ": ");
+				double cantidadNotas = scanner.nextDouble();
+				
+				// Indicar las Value(nota)
+				System.out.print("Ingrese la nota " + i + ": ");
+				double nota = scanner.nextDouble();
+
+				// Almacenar la nota en el HashMap
+				notasAlumnos.put(nota,cantidadNotas);
+			}
+
+			double sumaNotas = 0;
+
+			// Calcular la suma de las notas
+			for (double nota : notasAlumnos.values()) {
+				sumaNotas += nota;
+			}
+
+			double notaMedia = sumaNotas / totalNotas;
+
+			System.out.println("La nota media de los " + cantidadNotas + " alumnos es: " + notaMedia);
+
+			scanner.close();
 		}
-
-		double sumaNotas = 0;
-
-		// Calcular la suma de las notas
-		for (double nota : notasAlumnos.values()) {
-			sumaNotas += nota;
-		}
-
-		double notaMedia = sumaNotas / cantidadAlumnos;
-
-		System.out.println("La nota media de los " + cantidadAlumnos + " alumnos es: " + notaMedia);
-
-		scanner.close();
 	}
-}
