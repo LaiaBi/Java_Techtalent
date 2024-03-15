6d172846238dfc76852353a57e71c7256aeca470
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-03-15 11:49:58 +0100

    Update T8_2_Password.java

--- Prueba1/src/T8_POO/T8_2_Password.java
+++ Prueba1/src/T8_POO/T8_2_Password.java
@@ -1,5 +1,38 @@
 package T8_POO;
 
-public class T8_2_Password {
+import java.security.SecureRandom;
 
+public class T8_2_Password {
+	private static final String ALLOWED_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:',.<>?";
+	// Atributos
+	private String contraseña;
+	private int longitud;
+
+	// Constructores
+	public T8_2_Password() {
+		this.contraseña = "";
+		this.longitud = 0;
+	}
+
+	public T8_2_Password(String contraseña, int longitud) {
+		super();
+		this.contraseña = contraseña;
+		this.longitud = longitud;
+	}
+
+//Contraseña aleatoria
+	public static void main(String[] args) {
+		String password = generateRandomPassword(8);
+		System.out.println("Password: " + password);
+	}
+
+	public static String generateRandomPassword(int length) {
+		SecureRandom random = new SecureRandom();
+		StringBuilder sb = new StringBuilder(length);
+		for (int i = 0; i < length; i++) {
+			int randomIndex = random.nextInt(ALLOWED_CHARACTERS.length());
+			sb.append(ALLOWED_CHARACTERS.charAt(randomIndex));
+		}
+		return sb.toString();
+	}
 }
