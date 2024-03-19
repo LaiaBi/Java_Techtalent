70892711d8242bb48fedb77c13e81224a2fd613a
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-03-19 13:15:52 +0100

    actividad grupal2

---
+++ Prueba1/src/Trabajo2/Producto.java
@@ -0,0 +1,24 @@
+package Trabajo2;
+
+public class Producto {
+
+	private double precio;
+	private double iva;
+	private int cantidad;
+	private String nombre;
+
+	public Producto(double precio, double iva, int cantidad, String nombre) {
+		this.precio = precio;
+		this.iva = iva;
+		this.cantidad = cantidad;
+		this.nombre = nombre;
+	}
+
+	public Producto() {
+		this.precio = 1;
+		this.iva = 0.21;
+		this.cantidad = 10;
+		this.nombre = "";
+	}
+
+}
