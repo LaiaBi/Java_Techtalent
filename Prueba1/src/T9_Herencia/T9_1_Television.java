5918dff29614d13701a427e19f76efbe5501462f
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-03-27 13:57:17 +0100

    Create T9_1_Television.java

---
+++ Prueba1/src/T9_Herencia/T9_1_Television.java
@@ -0,0 +1,7 @@
+package T9_Herencia;
+
+public class T9_1_Television extends T9_1_Elecrtrodomestico1 {
+	// Atributos
+		protected double resolución;
+		protected final static double CARGA_PRED = 5;
+}
