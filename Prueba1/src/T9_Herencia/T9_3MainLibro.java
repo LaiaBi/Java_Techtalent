8ad2cded218f20142463e1bacd76c37519d98ddb
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-03-29 10:05:20 +0100

    Update T9_3MainLibro.java

--- Prueba1/src/T9_Herencia/T9_3MainLibro.java
+++ Prueba1/src/T9_Herencia/T9_3MainLibro.java
@@ -6,23 +6,26 @@ public class T9_3MainLibro {
 	// dos libros diferentes
 	public static void main(String[] args) {
 
-		Libro libroA = new Libro("9788401337208", "El nombre del viento", "Patrick Rothfuss", 880);
-		Libro libroB = new Libro("9780375869020", "Wonder", "Palacio, R. J.", 300);
+		T9_3Libro libroA = new T9_3Libro("9788401337208", "El nombre del viento", "Patrick Rothfuss", 880);
+		T9_3Libro libroB = new T9_3Libro("9780375869020", "Wonder", "Palacio, R. J.", 300);
 
-		System.out.println("---------------------------------");
-		System.out.println("··Primer libro··");
+		System.out.println("-------------------");
+		System.out.println("Libro 1");
 		System.out.println(libroA.toString());
 
-		System.out.println("----------------------------------");
-		System.out.println("··Segundo libro··");
+		System.out.println("------------------");
+		System.out.println("Libro 2");
 		System.out.println(libroB.toString());
-		// Comparación de cantidad de páginas
-		String libroConMasPaginas = (libroA.getNumPag() > libroB.getNumPag()) ? libroA.getTitulo()
-				: (libroA.getNumPag() < libroB.getNumPag()) ? libroB.getTitulo()
-						: "Los dos libros tienen el mismo número de página!";
+	}
 
-		System.out.println("------------------------------------");
-		System.out.println("··El libro con más páginas··");
-		System.out.println(libroConMasPaginas);
+	// Comparación de cantidad de páginas
+	public static T9_3Libro libroConMasPaginas(T9_3Libro libro1, T9_3Libro libro2) {
+		if (libro1.getNumeropaginas() > libro2.getNumeropaginas()) {
+			return libro1;
+		} else if (libro1.getNumeropaginas() < libro2.getNumeropaginas()) {
+			return libro2;
+		} else {
+			return null;
+		}
 	}
 }
