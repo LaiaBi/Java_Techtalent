b25dc1274271d0d5e38cf98fe02273a13cf3eaff
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-04-02 11:01:43 +0200

    Cine con asientos aleatorios

---
+++ Prueba1/src/T9_Herencia/T9_6Espectador.java
@@ -0,0 +1,42 @@
+package T9_Herencia;
+
+import java.util.Random;
+
+public class T9_6Espectador {
+	private String nombre;
+	private int edad;
+	private int dinero;
+
+	// Constructor
+	public T9_6Espectador(String nombre, int edad, int dinero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dinero = dinero;
+	}
+
+	//Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getDinero() {
+		return dinero;
+	}
+
+	public void setDinero(int dinero) {
+		this.dinero = dinero;
+	}
+
+}
