268d07be94044967d8f7c82ed99f4845ba802576
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-04-25 10:05:21 +0200

    Update base.sql

--- C3/GRUPAL2.0/base.sql
+++ C3/GRUPAL2.0/base.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE comercio_jalal 
+CREATE DATABASE comercio_jalal ;
 USE comercio_jalal; 
 
 CREATE TABLE proveedores(
@@ -8,7 +8,7 @@ CREATE TABLE proveedores(
 );
 
 CREATE TABLE productos(
-    id_producto CHAR(5), 
+    id_producto INT, 
     PRIMARY KEY (id_producto), 
     nombre VARCHAR(80), 
     precio FLOAT(5,2), 
@@ -20,16 +20,8 @@ CREATE TABLE productos(
     ON UPDATE CASCADE
 );
 
-CREATE TABLE clientes (
-    id_cliente INT AUTO_INCREMENT, 
-    PRIMARY KEY (id_cliente),
-    nombre VARCHAR(80), 
-    apellidos VARCHAR (80),
-    e_mail VARCHAR(150),
-);
-
 CREATE TABLE inventario(
-    id_producto CHAR(5), 
+    id_proveedor INT, 
     FOREIGN KEY (id_proveedor)
     REFERENCES proveedores (id_proveedor)
     ON DELETE CASCADE
@@ -41,3 +33,41 @@ CREATE TABLE inventario(
     ON DELETE CASCADE
     ON UPDATE CASCADE,
 );
+
+CREATE TABLE caja(
+    num_caja INT AUTO_INCREMENT, 
+    PRIMARY KEY (num_caja),
+    id_producto INT, 
+    FOREIGN KEY (id_producto)
+    REFERENCES productos (id_producto)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    cantidad INT NOT NULL, 
+    precio_producto FLOAT, 
+    IVA FLOAT(5,2) CHECK (IVA=0.04 OR IVA=0.21)
+    );
+
+CREATE TABLE ticket(
+    id_ticket INT AUTO_INCREMENT, 
+    PRIMARY KEY (id_ticket), 
+    num_caja INT, 
+    FOREIGN KEY (num_caja)
+    REFERENCES caja (num_caja)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    precio_total FLOAT, 
+    
+);
+
+CREATE TABLE clientes (
+    id_cliente INT AUTO_INCREMENT, 
+    PRIMARY KEY (id_cliente),
+    nombre VARCHAR(80), 
+    apellidos VARCHAR (80),
+    e_mail VARCHAR(150) UNIQUE NOT NULL, 
+    id_ticket INT, 
+    FOREIGN KEY (id_ticket)
+    REFERENCES ticket (id_ticket)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+); 
