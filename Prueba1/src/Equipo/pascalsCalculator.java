cf87509ed384ff7629a2d23ec87defb41fcc9194
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-05-08 13:16:53 +0200

    Create pascalsCalculator.java

---
+++ Prueba1/src/Equipo/pascalsCalculator.java
@@ -0,0 +1,15 @@
+package Equipo;
+import javax.swing.*;
+import java.awt.event.*;
+public class pascalsCalculator extends JFrame{
+private JTextField textField;
+    
+    public pascalsCalculator() {
+        setTitle("Pascal's_Calculator.exe");
+        setSize(400, 200);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+}
+}
