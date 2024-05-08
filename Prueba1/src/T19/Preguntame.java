d90e4756e35d664dcaff7ce3d56fa37a28ebbfeb
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-05-08 15:26:40 +0200

    upgrade act.3

--- Prueba1/src/T19/Preguntame.java
+++ Prueba1/src/T19/Preguntame.java
@@ -1,6 +1,11 @@
 package T19;
-import javax.swing.*;
-import java.awt.event.*;
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 public class Preguntame extends JFrame{
 	
 		private JTextField textField;
@@ -23,11 +28,34 @@ public class Preguntame extends JFrame{
 	     	panel.add(etiqueta);
 	    }
 	    // JRadioButton
-	    JPanel panel = new JPanel();
-	    JRadioButton windowsButton = new JRadioButton("Windows");
+	    JFrame frame = new JFrame("RadioButton Example");
+
+        JPanel panel = new JPanel();
+
+        JRadioButton windowsButton = new JRadioButton("Windows");
         JRadioButton linuxButton = new JRadioButton("Linux");
         JRadioButton macButton = new JRadioButton("Mac");
+
+        // Creamos un grupo para asegurar que solo se pueda seleccionar una opción a la vez
         ButtonGroup group = new ButtonGroup();
+        group.add(windowsButton);
+        group.add(linuxButton);
+        group.add(macButton);
+
+        panel.add(windowsButton);
+        panel.add(linuxButton);
+        panel.add(macButton);
+
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+	// Insertar texto para la accion
+		JLabel etiqueta = new JLabel("Especialidad:");
+		etiqueta.setBounds(100,20,160,20);
+		panel.add(etiqueta);
+	//
+
       
     public static void main(String[] args) {
         // Crear una instancia de la ventana
