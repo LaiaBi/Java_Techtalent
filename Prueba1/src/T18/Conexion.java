bfe0c79b444ea336ed2e998935ac9c5da0014171
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-05-13 13:22:28 +0200

    Update Conexion.java

--- Prueba1/src/T18/Conexion.java
+++ Prueba1/src/T18/Conexion.java
@@ -5,14 +5,15 @@ import java.sql.SQLException;
 public class Conexion {
 
 	public static void main(String[]args){
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conexion=DriverManager.getConnection("jdbc:mysql://localhost:3306","root","");
-			System.out.print("Conectado adecuadamente");
-		}catch (Exception ex)  {
-			System.out.print("Lo siento. No se puede conectar a la Base de datos");
-			System.out.print( ex );
-		}
+		 try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            Connection conexion=DriverManager.getConnection(
+	                    "jdbc:mysql://localhost:3306","root","");
+	            System.out.print("Server Connected");
+	        }catch(SQLException | ClassNotFoundException e) {
+	            System.out.print("Lo siento. No se puede conectar a la Base de datos");
+	            System.out.print(e);
+	        }
 	}
 	
 }
