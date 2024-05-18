4f21e021e5d71548d84d2db7729d2cf26796d0ad
LaiaBi <158473760+LaiaBi@users.noreply.github.com>
2024-05-18 13:02:13 +0200

    comandos
    Los comandos de los apuntes de programación.

---
+++ Prueba1/src/Apuntes/Vol1.java
@@ -0,0 +1,53 @@
+package Apuntes;
+public class Vol1 {
+	import java.util.Enumeration;
+	import java.util.Iterator;
+
+	package 0;
+	public class escritos {
+	int num [] = new int [10];
+	for (int i=0, multiplo=5; i<num.length;i++, multiplo+=5) {
+		num[i]= multiplo;
+		System.out.println(num[i]);
+	}
+	//ADD
+	lista.add(1);
+	lista.add(2);
+	lista.add(3);
+	System.out.println("ADD");
+	System.out.println(lista.toString());
+	//REMOVE
+	lista.remove(2);
+	System.out.println("REMOVE");
+	System.out.println(lista.toString());
+	//SIZE
+	System.out.println("SIZE");
+	System.out.println(lista.size());
+	//GET
+	System.out.println("GET");
+	System.out.println(lista.get(0));
+	//ITERATOR
+	System.out.println("ITERATOR");
+	Iterator<Integer> it = lista.iterator ();
+	int num;
+	while (it.hasNext()) {
+		num = it.next();
+		System.out.println(num);
+	}
+	//INDEXOF
+	System.out.println("INDEXOF");
+	System.out.println(lista.indexOf(1));
+	//CLEAR
+	lista.clear();
+	System.out.println("CLEAR");
+	System.out.println(lista.toString());
+	while (enumeration.hasMoreElements()) {
+		System.out.println(""+"hastable valores: " + Enumeration.nextElement());
+	}
+	---
+	Enumeration<String> llaves = contenedor.Key();
+	while (llaves.hasMoreElements()) {
+		System.out.println(""+"hastable llaves: " + llaves.nextElement());
+		}
+	}
+}
